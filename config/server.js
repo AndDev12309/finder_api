62f7a1f1cacf729d645ecc8acb108cb8649dba24
Andres Proaño <andrius12309@gmail.com>
2024-12-02 15:40:12 -0500

    fix

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("BASE_URL", "https://junction.proxy.rlwy.net:29506"),
+  url: env("BASE_URL", "https://finder-api-production-2024.up.railway.app"),
   app: {
     keys: env.array("APP_KEYS"),
   },
@@ -10,10 +10,9 @@ module.exports = ({ env }) => ({
   },
   sessions: {
     cookie: {
-      secure: env.bool("NODE_ENV", "production") === "production",
+      secure: true,
       httpOnly: true,
       sameSite: "strict",
     },
   },
-  proxy: true,
 });

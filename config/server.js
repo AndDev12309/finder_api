d11d86d39807abe61b15ed6c7c0b2ccc68ee166e
Andres Proaño <andrius12309@gmail.com>
2024-12-02 15:03:27 -0500

    fix 2

--- config/server.js
+++ config/server.js
@@ -1,11 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('BASE_URL', 'https://finder-api-production-2024.up.railway.app'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("BASE_URL", "https://finder-api-production-2024.up.railway.app"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  sessions: {
+    cookie: {
+      secure: env.bool("NODE_ENV", "production") === "production",
+      httpOnly: true, 
+      sameSite: "strict", 
+    },
   },
 });
